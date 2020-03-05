b511fc436875fd46ae766952a77d90d5ab11ad53
Olivier <olivier.gabison@gmail.com>
2020-03-04 21:05:13 -0600

    google maps heroku key

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
-import { googleMapsKey } from "../keys";
 // 725 x 450
 
 const position = {
